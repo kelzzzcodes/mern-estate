2626e131e6f441d36e4dadb34446b0d79eb1d451
Kelechi Otugbuali <hilakechi@gmail.com>
2023-12-22 23:01:32 +0100

    created pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { About, Home, Profile, SignIn, SignUp } from './pages'
 
 export default function App() {
-  return <div className='text-green-700'>App</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/sign-in" element={<SignIn/>} />
+        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/profile" element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+ import About from "./About";
+ import Home from "./Home";
+ import SignIn from "./SignIn";
+ import SignUp from "./SignUp";
+  import Profile from "./Profile";
+
+  export { Profile, About, SignUp, Home, SignIn };
