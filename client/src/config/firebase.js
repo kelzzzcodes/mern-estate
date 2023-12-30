42fd912670cd633b5a2074f939ccbddb5ce6e362
Kelechi Otugbuali <hilakechi@gmail.com>
2023-12-30 21:21:31 +0100

    complete profile image upload functionality

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -1,19 +1,21 @@
- import { getApps, getApp, initializeApp } from 'firebase/app'
- import { getAuth } from 'firebase/auth';
- import { getFirestore } from 'firebase/firestore';
+import { getApps, getApp, initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
-};
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
 
-const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
+const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig)
 
-const auth = getAuth(app);
-const db = getFirestore(app);
+const auth = getAuth(app)
+const db = getFirestore(app)
+const storage = getStorage(app)
 
-export { app, auth, db }
+export { app, auth, db, storage }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,75 @@
-import React from 'react'
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
+import { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { storage } from '../config/firebase'
 
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user)
+  const fileRef = useRef(null)
+  const [file, setFile] = useState(undefined)
+  const [filePercentage, setFilePercentage] = useState(0)
+  const [fileUploadError, setFileUploadError] = useState(false)
+  const [formData, setFormData] = useState({})
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file)
+    }
+  }, [file])
+
+  const handleFileUpload = (file) => {
+    const fileName = new Date().getTime() + file.name
+    const storageRef = ref(storage, fileName)
+    const uploadTask = uploadBytesResumable(storageRef, file)
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        setFilePercentage(Math.round(progress))
+      },
+      // eslint-disable-next-line no-unused-vars
+      (error) => {
+        setFileUploadError(true)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL }),
+        )
+      },
+    )
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
 
       <form className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          accept="image/*"
+          className="hidden"
+        />
         <img
-          src={currentUser.avatar}
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 cursor-pointer self-center mt-2"
         />
+        <p className='text-sm self-center'>
+          {fileUploadError ? (
+            <span className="text-red-700">Error Image Upload(image must be less than 10mb)</span>
+          ) : filePercentage > 0 && filePercentage < 100 ? (
+            <span className="text-slate-700">
+              {`Uploading ${filePercentage}`}
+            </span>
+          ) : filePercentage === 100 ? (
+            <span className="text-green-700">Image successfully upload</span>
+          ) : (
+            ''
+          )}
+        </p>
         <input
           type="text"
           placeholder="username"
